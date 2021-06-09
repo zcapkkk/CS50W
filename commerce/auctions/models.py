ed58b37b89d6dc47ca651e6e6d4086f5f99256f2
Kelvin <zcapkkk@ucl.ac.uk>
2021-06-09 17:23:33 +0800

    currently added a create listing form, which redirects to index page upon submission

---
+++ commerce/auctions/models.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.db.models.deletion import CASCADE
+
+
+class User(AbstractUser):
+    pass
+
+class Listing(models.Model):
+    name = models.CharField(name="Product Name", max_length=120)
+    seller = models.ForeignKey(User, on_delete=CASCADE)
+
+class Bid(models.Model):
+    price = models.DecimalField(name="Bidding Price", max_digits=20, decimal_places=2)
+    product = models.ForeignKey(Listing, on_delete=CASCADE)
+
+class Comment(models.Model):
+    product = models.ForeignKey(Listing, on_delete=CASCADE)
+    poster = models.ForeignKey(User, on_delete=CASCADE)
+    text = models.TextField(name="User Comment")
---
+++ commerce/auctions/views.py
@@ -0,0 +1,85 @@
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.forms.widgets import Textarea
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from django import forms
+from django.contrib.auth.decorators import login_required
+
+from .models import User
+
+class CreateForm(forms.Form):
+    name = forms.CharField(max_length=120, label="Product Name")
+    description = forms.CharField(label="Product Description", widget=Textarea)
+    price = forms.DecimalField(label="Starting Price", decimal_places=2, max_digits=20)
+
+
+
+def index(request):
+    return render(request, "auctions/index.html")
+
+
+def create(request):
+    # if request.method == "POST":
+    #     listingForm = CreateForm(request.POST)
+    #     if listingForm.is_valid():
+            
+
+
+
+    return render(request, "auctions/create.html", {"form": CreateForm()})
+
+    
+
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "auctions/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "auctions/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "auctions/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "auctions/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "auctions/register.html")
